ce0fe8584c93ea8cd2cea492f3d42aa1469e091f
Kushagra Gangwar <kushagra.mirdul15@gmail.com>
2025-02-21 00:26:57 +0530

    Your commit message here

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'kkushagrag/Portfolio.git', // Replace <repository-name> with the name of your GitHub repository
+  base: '', // Replace <repository-name> with the name of your GitHub repository
 });
